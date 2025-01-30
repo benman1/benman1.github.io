927d2339fd9a6255d9382970c8f9f9ae04053ff5
Ben Auffarth <auffarth@gmail.com>
2025-01-30 00:45:17 +0000

    sync loading of GA code; loading order

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4168534164"
+    serviceWorkerVersion: "151266355"
   }
 });
